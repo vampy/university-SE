73ff18567fb2d47f8f04ab4582fb6a10bf9c4656
Daniel Butum <danibutum@gmail.com>
2015-04-21 21:01:36 +0300

    Add role_id column to User for each type of user: - student - chief department - teacher - admin

--- school/user/models.py
+++ school/user/models.py
@@ -1,7 +1,18 @@
 from school.extensions import db
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, SmallInteger
 from flask.ext.login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class Role:
+    STUDENT = 1
+    TEACHER = 2
+    CHIEF_DEPARTMENT = 3
+    ADMIN = 4
+
+    @staticmethod
+    def get_roles():
+        return [Role.STUDENT, Role.TEACHER, Role.CHIEF_DEPARTMENT, Role.ADMIN]
 
 
 class User(UserMixin, db.Model):
@@ -12,9 +23,27 @@ class User(UserMixin, db.Model):
     realname = Column(String(128), nullable=True)
     email = Column(String(64), unique=True)
     password_hash = Column(String(160))
+    role_id = Column(SmallInteger, default=Role.STUDENT)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
+
+        # invalid role detected
+        if self.role_id not in Role.get_roles():
+            self.role_id = Role.STUDENT
+            print("ERROR: INVALID role_id")
+
+    def is_student(self):
+        return self.role_id == Role.STUDENT
+
+    def is_teacher(self):
+        return self.role_id == Role.TEACHER
+
+    def is_chief_department(self):
+        return self.role_id == Role.CHIEF_DEPARTMENT
+
+    def is_admin(self):
+        return self.role_id == Role.ADMIN
 
     @property
     def password(self):
