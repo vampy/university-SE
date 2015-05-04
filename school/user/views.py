3203c200162f27031dfcc63b3c73b1d4be5961c7
sonyk <sony.peung@gmail.com>
2015-05-04 18:41:35 +0300

    created seeCourses.html add some modifications on user.views.py and user.index.html

--- school/user/views.py
+++ school/user/views.py
@@ -26,6 +26,49 @@ def changepassword():
     return render_template('user/changepassword.html', form=form)
 
 
+@user.route('/seeCourses')
+@login_required
+def seeCourses():
+    courses = [
+                {
+                    'name': 'Operating system'
+                },
+                {
+                    'name': 'Software design'
+                },
+                {
+                    'name': 'Software engineering'
+                },
+                {
+                    'name': 'Software quality'
+                }
+        ]
+
+    projects = [
+        {
+            'name': 'Test 1',
+            'deadline': '2015-03-20',
+            'grade': '10'
+        },
+        {
+            'name': 'Project 1',
+            'deadline': '2015-03-27',
+            'grade': '9'
+        },
+        {
+            'name': 'Exam mid term',
+            'deadline': '2015-04-10',
+            'grade': '8'
+        },
+        {
+            'name': 'Project 2',
+            'deadline': '2015-05-10',
+            'grade': '10'
+        }
+    ]
+
+    return render_template("user/seeCourses.html",courses=courses, projects=projects)
+
 # TODO
 def settings():
     pass
