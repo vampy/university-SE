b471451ebf0e8c320dfbc293a10e0c004a20dfca
matthias_b <matthias.beulin@gmail.com>
2015-05-14 15:19:44 +0300

    see_courses modification Done waiting the link between CD and Departements #2 MERGE problem resolved

--- school/user/views.py
+++ school/user/views.py
@@ -7,8 +7,6 @@ from school.decorators import role_required
 from school.models import *
 
 user = Blueprint('user', __name__)
-course = Blueprint('course', __name__)
-
 
 # the root of our website
 @user.route('/')
@@ -31,10 +29,6 @@ def change_password():
         return form.redirect("user.index")
 
     return render_template('user/change_password.html', form=form)
-
-
-
-
 
 
 @user.route('/user/<int:user_id>', methods=['GET', 'POST'])
@@ -92,47 +86,6 @@ def users():
 
     users_list = User.query.all()
     return render_template("user/users.html", users=users_list, add_form=form)
-
-
-
-@user.route('/upload_course_results', methods=['GET', 'POST'])
-@login_required
-def upload_course_results():
-    le_semestre = ""
-    le_cours = ""
-    semesters = []
-    the_semester = Semester.query.all()
-    for semester in the_semester:
-        semesters.append(semester)
-
-    allcourses = [teach.course for teach in current_user.teaches.all()]
-
-    if request.method == 'POST':
-        val=request.form['course']
-        sem=request.form['semester']
-        students = []
-        grades = []
-
-        le_cours = Course.query.filter_by(id=int(val)).first()
-        le_semestre = Semester.query.filter_by(id=sem).first()
-
-        studentsenrolled = Enrollment.query.filter_by(course_id=int(val),semester_id=int(sem)).all()
-        for student in studentsenrolled:
-            students.append(User.query.filter_by(id=student.student_id).first())
-            grades.append(student.grade)
-
-        return render_template("user/upload_course_results.html",
-                               allcourses=allcourses,
-                               thecourse=le_cours,
-                               students=students,
-                               semesters=semesters,
-                               le_semestre=le_semestre,
-                               grades=grades)
-    else :
-        return render_template("user/upload_course_results.html",allcourses=allcourses,
-                               le_semestre=le_semestre,
-                               semesters=semesters,
-                               thecourse=le_cours)
 
 
 @user.route('/upload_course_results/<int:user_id>,<int:course_id>,<int:grade>,<int:semester_id>')
