3338603a4ad3714c8b051d0d6e18b8d7b95e07b1
Daniel Butum <danibutum@gmail.com>
2015-05-29 18:15:50 +0300

    Add more validation to the courses form. Accept only alphanumeric course names

--- school/course/forms.py
+++ school/course/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import Form
 from wtforms import SubmitField, BooleanField, StringField, IntegerField, SelectField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, Regexp, NumberRange
 
 
 class EditCourseForm(Form):
@@ -12,7 +12,7 @@ class AddCourseForm(Form):
 
 
 class TeacherAddCourseForm(Form):
-    name = StringField("Name", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired(), Length(min=4, max=64), Regexp(r'^[a-zA-Z0-9 ]+$')])
     degree_id = SelectField("Degree", coerce=int, validators=[DataRequired()], choices=[])
     semester_id = SelectField("Semester", coerce=int, validators=[DataRequired()], choices=[])
 
@@ -24,7 +24,7 @@ class CDEditCourseForm(TeacherAddCourseForm):
                            choices=[(i, i) for i in range(2, 6)])
     min_students = IntegerField("Min Students", validators=[DataRequired()])
     max_students = IntegerField("Max Students", validators=[DataRequired()])
-    credits = IntegerField("Credits", validators=[DataRequired()])
+    credits = IntegerField("Credits", validators=[DataRequired(), NumberRange(min=1, max=10)])
     approval_reason = TextAreaField("Reason")
     is_approved = BooleanField("Approved")
 
